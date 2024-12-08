96aff7b2d0242233cd90fa12966be37a31fe8153
mesi <mesicharida202@gmail.com>
2024-12-08 22:09:50 +0900

    tasks26

---
+++ src/bin/tasks26.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    let inputs: Vec<String> = input
+        .trim()
+        .split_whitespace()
+        .map(|i| i.parse().unwrap())
+        .collect();
+
+    let s = &inputs[0];
+    let t = &inputs[1];
+    let u = &inputs[2];
+
+    println!("{}{}{}", u, t, s);
+}
